59b31de11ac885ae90e7631874f4116233528084
Darius Perry <DarJPerry@gmail.com>
2018-04-03 13:44:26 -0400

    More bug fixes and added to catalog data

--- UI/modifyStudent.php
+++ UI/modifyStudent.php
@@ -5,7 +5,6 @@ $studentId    = $_REQUEST['student_id'];
 
 if (file_exists("../students/" . $studentId . ".sa")) {
         $fp = fopen("../students/" . $studentId  . ".sa", 'wb');
-
         fwrite($fp, $studentInfo);
     echo "Your New Student Has Been Saved";
     
