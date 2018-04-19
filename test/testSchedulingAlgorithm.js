5dd1269a8818b8da7ee32d07be8ece542df4a9a1
DJD <g>
2018-04-19 17:25:17 -0400

    Added unit test for CriticalPath.

--- test/testSchedulingAlgorithm.js
+++ test/testSchedulingAlgorithm.js
@@ -15,7 +15,8 @@ describe('SchedulingAlgorithm', function() {
     });
 
     describe('Backflow', function() {
-        it('should take a dictionary of sorted course numbers, each paired with a placeholder priority, and then figure the actual priority for each course.', function() {
+        it('should take a dictionary of sorted course numbers, each paired with a placeholder priority, and then figure ' +
+            'the actual priority for each course.', function() {
             prioritizedCourses = backflow([{'CS121': 999}, {'CS124': 999}, {'MATH161': 999}, {'CS222': 999}, {'MATH162': 999}]);
             assert.deepEqual(prioritizedCourses, [{'CS121': 1}, {'CS124': 1}, {'MATH161': 1}, {'CS222': 0}, {'MATH162': 0}]);
         });
@@ -25,8 +26,10 @@ describe('SchedulingAlgorithm', function() {
     });
 
     describe('CriticalPath', function() {
-        it('should do something', function() {
-            assert.deepEqual(1, 1);
+        it('should take a dictionary of sorted course numbers, each paired with their figured priorities, and then find ' +
+            'the courses that should be taken each semester for the optimal schedule.', function() {
+            optimizedSchedule = critPath([{'CS121': 1}, {'CS124': 1}, {'MATH161': 1}, {'CS222': 0}, {'MATH162': 0}]);
+            assert.deepEqual(optimizedSchedule, [['CS121', 'CS124', 'MATH161'], ['CS222', 'MATH162']]);
         });
         // it('should do something else', function() {
         //     assert.equal(2, 2);
