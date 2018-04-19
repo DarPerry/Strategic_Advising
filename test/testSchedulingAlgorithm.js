3af94ca1cc2e5b923c1f79aeccbd686b0152cc0c
DJD <g>
2018-04-19 16:01:45 -0400

    Added placeholder code for unit tests.

---
+++ test/testSchedulingAlgorithm.js
@@ -0,0 +1,33 @@
+// Enter "npm test" into the terminal to run tests.
+
+var assert = require('assert');
+
+describe('SchedulingAlgorithm', function() {
+
+    describe('TopologicalSort', function() {
+        it('should do something', function() {
+            assert.equal(1, 1);
+        });
+        it('should do something else', function() {
+            assert.equal(2, 2);
+        });
+    });
+
+    describe('Backflow', function() {
+        it('should do something', function() {
+            assert.equal(1, 1);
+        });
+        it('should do something else', function() {
+            assert.equal(2, 2);
+        });
+    });
+
+    describe('CriticalPath', function() {
+        it('should do something', function() {
+            assert.equal(1, 1);
+        });
+        it('should do something else', function() {
+            assert.equal(2, 2);
+        });
+    });
+});
