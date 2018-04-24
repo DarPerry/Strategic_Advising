bdb83b616ea7c668371f62c4a562b12db237cbb3
ashking42 <ashlyn.king42@gmail.com>
2018-04-23 22:21:28 -0400

    Adding more logic/functions to ScheduleGenerator Not yet tested

--- resources/js/ScheduleGenerator.js
+++ resources/js/ScheduleGenerator.js
@@ -5,28 +5,58 @@ Description: This class prioritizes courses based on prerequisites
  */
 
 var courses = [];
+//courses grouped by priorities
 
 
-//add courses to course dictionary, intializing priority with 0
+//add courses to course dictionary, intializing with prerequisites and priority of 0
 function initalizeCourses(){
-    $.ajax({
-        type: "GET",
+    $.get({
         url: 'resources/php/readCourseCatalog.php',
-        data: {
-        //add data here
-        },
-        async: false,
+        dataType: '.cca',
         success: function (data) {
-            //on success
+            for(var i in data){
+                courses.add(new Course(i[0], i[4], i[2], 0));
+            }
         }
     })
 }
 
 //iterate through courses and assign proper priority
+function prioritize(){
+    var highestPriority = 0;
+    for(var course in courses){
+        if(course.getPrereqs().length == 0){
+            ;
+        } else {
+            for(var pre in  course.getPrereqs()){
+                if(course.getPrereqs()[pre] > highestPriority){
+                    highestPriority = course.getPrereqs()[pre] + 1;
+                }
+            }
+        }
+    }
+}
 
-function Course(){
-    this.preReqs = [];
-    this.priority = 0;
+function schedule(){
+    var highestPriority = 0;
+    var schedule = []
+    while(schedule.length != courses.length){
+        for(var course in courses){
+            if(course.getPriority() == highestPriority){
+                schedule.add(course);
+                courses.remove(course);
+            }
+        }
+        highestPriority ++;
+    }
+}
+
+
+function Course(courseName, preReqs, creditHours, priority){
+    this.preReqs = preReqs;
+    this.priority = priority;
+    this.courseName = courseName;
+    this.creditHours = creditHours;
 }
 
 Course.prototype.getPrereqs = function(){
