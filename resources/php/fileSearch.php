86376c9617f84463a1a921a83f1cafe1a9cdb041
codyyork <clyork@bsu.edu>
2018-04-26 15:57:30 -0400

    Added the class headers. Just need to go in and add the reviewers name and date reviewed.

--- resources/php/fileSearch.php
+++ resources/php/fileSearch.php
@@ -1,3 +1,12 @@
+/*
+Name :  File_Search_PHP
+Author:  Darius Perry
+Created: February 20, 2018
+Review :
+Description: This class is the PHP logic for searching through student files.
+*/
+
+
 <?php
 $fileName = $_REQUEST["id"];
 $fileToSearch = "../data/students/" . $fileName . ".sa";
