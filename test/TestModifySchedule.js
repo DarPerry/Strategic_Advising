431f0e422aebdabd2b83d992eb6cac0e64984168
DJD <g>
2018-04-30 17:31:39 -0400

    Fixed TestModifySchedule.

--- test/TestModifySchedule.js
+++ test/TestModifySchedule.js
@@ -4,19 +4,17 @@ const fs = require('fs');
 const assert = require('assert');
 const studentID = 987654321;
 const studentJSON = JSON.parse(fs.readFileSync('./resources/data/students/' + studentID + '.sa', 'utf-8'));
-// const scheduleString = studentJSON['schedule'].toString();
+const scheduleString = JSON.stringify(studentJSON['schedule']);
 const removedCourse = "CS120";
 const addedCourse = "CS333";
 
-console.log(studentJSON["schedule"]["year"]);
-
-// describe('ModifySchedule', function() {
-//     describe('NewSchedule', function() {
-//         it('should not contain removed course.', function() {
-//             assert.equal(scheduleString.search(removedCourse), -1);
-//         });
-//         it('should contain added course.', function() {
-//             assert.equal(scheduleString.search(addedCourse), -1);
-//         });
-//     });
-// });
+describe('ModifySchedule', function() {
+    describe('NewSchedule', function() {
+        it('should not contain removed course.', function() {
+            assert.equal(scheduleString.search(removedCourse), -1);
+        });
+        it('should contain added course.', function() {
+            assert.notEqual(scheduleString.search(addedCourse), -1);
+        });
+    });
+});
